27cd9ab654ad618e58a785d8eb8679bd8bf737d6
Andoni Perales <94477827+andoniperales@users.noreply.github.com>
2022-08-23 20:09:52 +0200

    Update dle.py

--- dle.py
+++ dle.py
@@ -2,42 +2,44 @@
 
 from sys import argv
 from fake_headers import Headers
-from selenium import webdriver
+from selenium.webdriver import Chrome as chrome, ChromeOptions as Options
 from selenium.webdriver.common.by import By
 
-def main():	
-	# Generate fake user-agent (the site rejects headless operations otherwise)
-	
-	header = Headers(
-		browser="chrome",
-		os="win",
-		headers=False
-	)
-	
-	UA = header.generate()['User-Agent']
-	
-	# Set webdriver options 
-	
-	opt = webdriver.ChromeOptions()
-	opt_list = [
-		f'user-agent={UA}', 
-		'--headless', 
-		'--disable-gpu', 
-		'--allow-running-insecure-content'
-	]
-	for o in opt_list:
-		opt.add_argument(f"{o}")
-	driver = webdriver.Chrome(options=opt)
-	driver.implicitly_wait(5)
 
-	# Get & print term's information
-	
-	entrada = argv[1]
-	driver.get(f"https://dle.rae.es/{entrada}")
-	definicion = driver.find_element(By.CSS_SELECTOR, "div#resultados")	
-	print(definicion.text)
+def main(entrada):	
+    # Generate fake user-agent (the site rejects headless operations otherwise)
 
-	driver.quit()
+    header = Headers(
+        browser="chrome",
+        os="win",
+        headers=False
+    )
+    
+    UA = header.generate()['User-Agent']
+	
+    # Set webdriver options 
+    
+    options = Options()
+    options_list = [
+        f'user-agent={UA}', 
+        '--headless', 
+        '--disable-gpu', 
+        '--allow-running-insecure-content'
+        ]
+    for parameter in options_list:
+        options.add_argument(f"{parameter}")
+
+# Get & print term's information
+    
+    with chrome(options=options) as driver:
+        driver.implicitly_wait(5)
+        driver.get(f"https://dle.rae.es/{entrada}")
+        definicion = driver.find_element(By.CSS_SELECTOR, "div#resultados")	
+        print(definicion.text)  
+
+def definicion(palabra):
+        main(palabra)
 
 if __name__ == '__main__':
-	main()
+    entrada = argv[1]
+    main(entrada)
