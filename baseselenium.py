333de2aadfda836528bbd4a7a4e12e8386e12cf8
Andoni <aferpe@pm.me>
2024-02-29 22:14:43 +0100

    .

---
+++ baseselenium.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from fake_headers import Headers
+from selenium.webdriver import Firefox as firefox, FirefoxOptions as Options
+
+def generate():
+	# Generate fake user-agent (the site rejects headless operations otherwise)
+	header = Headers(
+		browser="firefox",
+		os="win",
+		headers=False
+	)
+	
+	UA = header.generate()['User-Agent']
+	
+	# Set webdriver options 
+	options = Options()
+	options_list = [
+		f'user-agent={UA}', 
+		'--headless', 
+		'--disable-gpu', 
+		'--allow-running-insecure-content'
+	]
+
+	for parameter in options_list:
+		options.add_argument(f"{parameter}")
+
+	driver = firefox(options=options) 
+	return driver
--- dle.py
+++ dle.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python
 
-import os
-from fake_headers import Headers
-from selenium.webdriver import Firefox as firefox, FirefoxOptions as Options
+import os, re
+import baseselenium
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 def definicion(term):
-	# Generate fake user-agent (the site rejects headless operations otherwise)
-	header = Headers(
-		browser="firefox",
-		os="win",
-		headers=False
-	)
+	driver = baseselenium.generate()
 	
-	UA = header.generate()['User-Agent']
-	
-	# Set webdriver options 
-	options = Options()
-	options_list = [
-		f'user-agent={UA}', 
-		'--headless', 
-		'--disable-gpu', 
-		'--allow-running-insecure-content'
-	]
-
-	for parameter in options_list:
-		options.add_argument(f"{parameter}")
-
 	# Get & print term's information
-	with firefox(options=options) as driver:
+	try:
 		driver.implicitly_wait(5)
 		driver.get(f"https://dle.rae.es/{term}")
 		resultados = driver.find_element(By.TAG_NAME, "article").text
-		return resultados	
+	except NoSuchElementException as error:
+		print("Term doesn't exist. Looking for similar ones...")
+		try:
+			driver.implicitly_wait(5)
+			driver.find_element(By.CLASS_NAME, "n1")
+		except NoSuchElementException as error:
+			print("There are no similar terms. Exiting...")
+			driver.quit()
+		else:
+			similar_terms = [re.search(r"\w+", word.text).group(0) for word in driver.find_elements(By.CLASS_NAME, "n1")]
+			if len(similar_terms) == 1:
+				print(f"There's one similar term: \"{similar_terms[0]}\"")
+				choice = input("Do you want to check its definition? (y/n)\n")
+				if "y" in choice:
+					definicion(f"{similar_terms[0]}")
+					driver.quit()
+				else:
+					driver.quit()
+			else:
+				print("Choose a similar term:")
+				for n in range(len(similar_terms)):
+					print(f"{n+1} {similar_terms[n]}")
+				choice = input("")
+				definicion(f"{similar_terms[int(choice) - 1]}")
+	else:
+		print(resultados)
 		driver.quit()
 
 if __name__ == '__main__':
 	term = input('Search for a term\n')
-	os.system('clear')
-	print(definicion(term))
+	definicion(term)
