01dff33dc636e38ee02a85c1d991e9805f3c8625
Andoni Perales <94477827+andoniperales@users.noreply.github.com>
2024-12-10 02:36:04 +0100

    Update dle.py

--- dle.py
+++ dle.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 def definicion(term):
-	driver = baseselenium.generate()
+	driver = baseselenium.fakeit()
 	
 	# Get & print term's information
 	try:
